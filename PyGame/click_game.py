900c82c47150079dddb9061598ef09470d5591c0
Languroo <113522010+Languroo@users.noreply.github.com>
2023-02-10 08:56:02 -0500

    Add files via upload

--- PyGame/click_game.py
+++ PyGame/click_game.py
@@ -12,3 +12,12 @@ def update():
     blob.left = blob.left + 2
     if blob.left > WIDTH:
         blob.right = 0
+def on_mouse_down(pos):
+    if blob.collidepoint(pos):
+        set_blob_hurt()
+def set_blob_hurt():
+    blob.image = 'hurt'
+    sounds.eep.play()
+    clock.schedule_unique(set_blob_normal, 1.0)
+def set_blob_normal():
+    blob.image = 'draft'
